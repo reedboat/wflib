7c428c1bc96f8bc3aeefb2e5bff9112e51b40819
reedboat <zhqm03@gmail.com>
2012-10-05 00:34:51 -0700

    fetch keys

--- Parameter.class.php
+++ Parameter.class.php
@@ -13,13 +13,31 @@ class WF_Parameter {
         }
     }
 
+    /**
+     * fetch 
+     * 
+     * @param mixed $data 原数组
+     * @param mixed $keys 如果是普通数组，则直接获取这些keys。如果是关联数组，则还会有改名处理
+     * @param mixed $filter_func 过滤函数, 可使用intval等内置函数，也可使用自定义函数，方法等。
+     * @access public
+     * @return void
+     */
     public function fetch($data, $keys, $filter_func=null){
         $result = array();
-        foreach($keys as $key){
-            $result[$key] = isset($data[$key]) ? $data[$key] : null;
-            if ($filter_func && is_string($result[$key])){
-                $result[$key] = $filter_func($result[$key]);
+        $flag = array_values($keys) == $keys;
+        foreach($keys as $key => $default){
+            if ($flag){
+                $key = $default;
+                $value = isset($data[$key]) ? $data[$key] : null;
             }
+            else {
+                $value = isset($data[$key]) ? $data[$key] : $default;
+            }
+
+            if (!is_null($filter_func) && isset($data[$key]) && !is_array($data[$key])) {
+                $value = call_user_func($filter_func, $value);
+            }
+            $result[$key] = $value;
         }
         return $result;
     }
@@ -45,7 +63,5 @@ class WF_Parameter {
     public function cookie($key, $default=null){
         return $this->retrieve($_COOKIE, $key, $default, $false);
     }
-
-
 }
 ?>
