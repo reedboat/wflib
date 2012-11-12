ff3cddd503a148148cc82991322a636f9ff1c65d
reedboat <zhqm03@gmail.com>
2012-11-12 14:34:40 +0800

    update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -20,4 +20,37 @@ function _log($msg){
 function _str(){
     return call_user_func_array('sprintf', func_get_args());
 }
+
+require __DIR__ . '/Loader.class.php';
+WF_Loader::registerAutoLoad();
+
+/**
+ * WF 常用功能的快捷操作类
+ */
+class WF {
+    public function reg($key){
+        return WF_Registry::get($key);
+    }
+
+    public function ini($key, $value=null){
+        return WF_Config::get($key);
+    }
+
+    public function post($url, $params){
+        $http = new WF_Http();
+        return $http->request($url, $params, array('method'=>'post'));
+    }
+
+    public function log($msg, $level='INFO'){
+        $logger = self::get('logger');
+        $logger->log($msg, $level);
+    }
+
+    public function dump($msg){
+        $logger = self::get('logger');
+        $logger->dump($msg);
+    }
+    //fire
+    //fetch
+}
 ?>
